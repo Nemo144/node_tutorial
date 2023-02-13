3d0a794021f7ae499b0e36478c2f40609d66d3b2
Nnamdi Okeke <onnamdialex@gmail.com>
2023-02-13 22:50:06 +0100

    :zap: improve performance
    installed the express npm for better server experience!

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-//to create a server
-const http = require("http");
-const server = http.createServer((req, res) => {
-  res.end("hello world from node js");
+//to create a server using the "express" package.
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
+app.listen(3000);
+
+//to create a server using the http module
+// const http = require("http");
+// const server = http.createServer((req, res) => {
+//   res.end("hello world from node js");
+// });
+
 //to access the server via localhost
-server.listen(3000);
+// server.listen(3000);
 
-const { sum } = require("./helpers");
+// const { sum } = require("./helpers");
 
-const total = sum(10, 200);
-console.log("Total:", total);
+// const total = sum(10, 200);
+// console.log("Total:", total);
