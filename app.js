40214133531eeee00228ef166c708a939bc55748
Nnamdi Okeke <onnamdialex@gmail.com>
2023-02-14 19:33:04 +0100

    :zap: testing the file system module of nodejs

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 //to create a server using the "express" package.
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
-app.listen(3000);
-
-//to create a server using the http module
-// const http = require("http");
-// const server = http.createServer((req, res) => {
-//   res.end("hello world from node js");
+// app.get("/", (req, res) => {
+//   res.send("Hello world");
 // });
 
-//to access the server via localhost
-// server.listen(3000);
+// app.listen(3000);
 
-// const { sum } = require("./helpers");
+//calling the file system(fs) module of nodejs
+const fs = require("fs");
+const fileName = "target.txt";
 
-// const total = sum(10, 200);
-// console.log("Total:", total);
+//to watch for changes in the target.txt file
+fs.watch(fileName, () => console.log(`File Changed!`));
