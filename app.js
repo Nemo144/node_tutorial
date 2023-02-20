b63957eef8476c11ca3e6aa87e4ecf9d98255194
Nnamdi Okeke <onnamdialex@gmail.com>
2023-02-20 20:44:46 +0100

    :art: refactoring code

--- app.js
+++ app.js
@@ -2,13 +2,16 @@
 const fs = require("fs");
 const fileName = "target.txt";
 
+//creating a function to handle the error in the readFile fs method
+const errHandler = (err) => console.log(err);
+
+//creating a function to handle the success outcome
+const dataHandler = (data) => console.log(data.toString());
+
 // to read a file with the readFile fs module asynchronously
 fs.readFile(fileName, (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data.toString());
-  }
+  if (err) errHandler(err);
+  dataHandler(data);
 });
 
 console.log("Node js programming");
