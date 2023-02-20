5e7880bef6ee3a7de0d2da836fa8dbef162ca9fe
Nnamdi Okeke <onnamdialex@gmail.com>
2023-02-20 20:18:57 +0100

    :construction: nodetut wip ...
    asynchronous node programming...

--- app.js
+++ app.js
@@ -13,4 +13,15 @@ const fs = require("fs");
 const fileName = "target.txt";
 
 //to watch for changes in the target.txt file
-fs.watch(fileName, () => console.log(`File Changed in target.txt!`));
+// fs.watch(fileName, () => console.log(`File Changed in target.txt!`));
+
+//to read a file with the readFile fs module
+fs.readFile(fileName, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
+});
+
+console.log("Node js programming");
